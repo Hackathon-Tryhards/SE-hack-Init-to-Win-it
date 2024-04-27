cf477e5c9f20dcef9294888dc926d024c5cf1913
Asim Shah <asimshah2111@gmail.com>
2024-04-27 12:09:30 +0530

    added web sockets

--- server/server.js
+++ server/server.js
@@ -21,9 +21,28 @@ import  corsOptions  from './config/corsOptions.js'
 import  connectToDB  from './services/mongooseService.js'
 import loginRouter from './routers/loginRoute.js'
 import registerRouter from './routers/registerRoute.js'
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express()
 const PORT = 3000
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PATCH"],
+    },
+});
+
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+function emitChanges(endpoint, payload) {
+    io.emit(endpoint, { payload });
+}
 
 connectToDB()
 
