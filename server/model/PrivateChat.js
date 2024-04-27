2dd27375554d1640fac26c6de6edf2d8c8696430
Asim Shah <asimshah2111@gmail.com>
2024-04-27 15:32:52 +0530

    added chat schame

---
+++ server/model/PrivateChat.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const PrivateChatSchema = new Schema(
+  {
+    chat_id: {
+      type: String,
+      required: true,
+    },
+    participants: {
+      type: [String],
+      default: [],
+    },
+    messages: {
+      type: [
+        {
+          timestamp: { type: String },
+          sender: { type: String },
+          content: { type: String },
+        },
+      ],
+    },
+  },
+  { collection: "privateChats" }
+);
+
+const PrivateChat = mongoose.model("PrivateChat", PrivateChatSchema);
+
+export default PrivateChat;
