766ce15ef60abf66e94adffc728bf344a73b37ae
unknown <ninadmaadhavi13@gmail.com>
2024-04-28 08:24:50 +0530

    set goal is working

--- client/src/pages/SetGoal.jsx
+++ client/src/pages/SetGoal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { format, differenceInMinutes } from 'date-fns';
 import axios from 'axios';
 import './SetGoal.css';
@@ -6,23 +6,7 @@ import './SetGoal.css';
 const GoalSetter = () => {
     const [goal, setGoal] = useState('');
     const [deadline, setDeadline] = useState(new Date());
-    const [goals, setGoals] = useState([]);
     const userData = JSON.parse(localStorage.getItem('userData'));
-
-    useEffect(() => {
-        // Fetch goals from the backend when the component mounts
-        fetchGoals();
-    }, []);
-
-    const fetchGoals = async () => {
-        try {
-            const response = await axios.get(`http://localhost:3000/goals/${userData.username}`);
-            setGoals(response.data.goals);
-        } catch (error) {
-            console.error('Error fetching goals:', error);
-            // Optionally, handle error scenarios here
-        }
-    };
 
     const handleGoalChange = (e) => {
         setGoal(e.target.value);
@@ -46,7 +30,6 @@ const GoalSetter = () => {
             });
             console.log('Goal set:', response.message);
             // Fetch goals again after adding a new one
-            fetchGoals();
         } catch (error) {
             console.error('Error setting goal:', error);
             // Optionally, handle error scenarios here
@@ -68,15 +51,6 @@ const GoalSetter = () => {
                 </div>
                 <button type="submit">Set Goal</button>
             </form>
-
-            <div className="goals-list">
-                <h2>Goals:</h2>
-                <ul>
-                    {goals.map((goal, index) => (
-                        <li key={index}>{goal}</li>
-                    ))}
-                </ul>
-            </div>
         </div>
     );
 };
