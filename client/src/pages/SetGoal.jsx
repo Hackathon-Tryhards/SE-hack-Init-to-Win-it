bb853a65b0ed641d024816b2ae589fe1928d81b2
SoulDev07 <mayankravariya@gmail.com>
2024-04-28 09:32:41 +0530

    add goal setter to tools

--- client/src/pages/SetGoal.jsx
+++ client/src/pages/SetGoal.jsx
@@ -5,7 +5,7 @@ import './SetGoal.css';
 
 const GoalSetter = () => {
     const [goal, setGoal] = useState('');
-    const [deadline, setDeadline] = useState(new Date());
+    const [deadline, setDeadline] = useState(format(new Date(), "yyyy-MM-dd'T'HH:mm"));
     const userData = JSON.parse(localStorage.getItem('userData'));
 
     const handleGoalChange = (e) => {
@@ -13,13 +13,12 @@ const GoalSetter = () => {
     };
 
     const handleDeadlineChange = (e) => {
-        setDeadline(new Date(e.target.value));
+        setDeadline(e.target.value);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // Format the deadline in the required format
-        const minutesDifference = differenceInMinutes(deadline, new Date());
+        const minutesDifference = differenceInMinutes(new Date(deadline), new Date());
         console.log('Minutes difference:', minutesDifference);
 
         try {
@@ -28,8 +27,9 @@ const GoalSetter = () => {
                 goal,
                 time: minutesDifference,
             });
-            console.log('Goal set:', response.message);
-            // Fetch goals again after adding a new one
+            setGoal('');
+            console.log('Goal set:', response.data);
+            // Optionally, fetch goals again after adding a new one
         } catch (error) {
             console.error('Error setting goal:', error);
             // Optionally, handle error scenarios here
@@ -37,19 +37,48 @@ const GoalSetter = () => {
     };
 
     return (
-        <div className="goal-setter">
-            <form className="add-form" onSubmit={handleSubmit}>
-                <div className="form-control">
-                    <label>Name</label>
-                    <input
-                        type="text"
-                        placeholder='Enter your goal'
-                        value={goal}
-                        onChange={handleGoalChange}
-                    />
-                    <input type="datetime-local" id="deadline" value={format(deadline, "yyyy-MM-dd'T'HH:mm")} onChange={handleDeadlineChange} />
+        <div className="container mx-auto flex justify-center items-center h-screen">
+            <form className="w-full max-w-sm border-4 border-maingreen p-4 rounded-lg text-lg">
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+                            Enter your Goal
+                        </label>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            id="inline-full-name"
+                            type="text"
+                            value={goal}
+                            onChange={handleGoalChange} />
+                    </div>
                 </div>
-                <button type="submit">Set Goal</button>
+                <div className="md:flex md:items-center mb-6">
+                    <div className="md:w-1/3">
+                        <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-deadline">
+                            Deadline
+                        </label>
+                    </div>
+                    <div className="md:w-2/3">
+                        <input
+                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                            id="inline-deadline"
+                            type="datetime-local"
+                            onChange={handleDeadlineChange}
+                            value={deadline}
+                        />
+                    </div>
+                </div>
+                <div className="md:flex md:items-center">
+                    <div className="md:w-1/3"></div>
+                    <div className="md:w-2/3">
+                        <button className="shadow bg-maingreen hover:bg-maingreen focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="button"
+                            onClick={handleSubmit}>
+                            Add Goal
+                        </button>
+                    </div>
+                </div>
             </form>
         </div>
     );
